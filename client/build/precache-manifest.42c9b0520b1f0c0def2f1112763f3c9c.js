689689a958ed3613dc7d913f653bc53b60b69d6f
Chris Carreon <mrwiggles6545@yahoo.com>
2020-09-17 17:38:14 -0500

    .txt

---
+++ client/build/precache-manifest.42c9b0520b1f0c0def2f1112763f3c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55eb9b2822c9d26bbdd6",
+    "url": "/static/css/main.8801f782.chunk.css"
+  },
+  {
+    "revision": "55eb9b2822c9d26bbdd6",
+    "url": "/static/js/main.d2d72e49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "752083936c9510c13a40",
+    "url": "/static/js/2.12c04363.chunk.js"
+  },
+  {
+    "revision": "987be20fd0e8224f033856617750d1b5",
+    "url": "/index.html"
+  }
+];
